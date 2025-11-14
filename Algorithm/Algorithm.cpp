3cf97e88b98ceb5bacf0dce8b360e6c0f4c9e92c
Karonzer <knkhsu122@naver.com>
2025-11-14 19:42:09 +0900

    유클리드 호재법

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,11 +1,71 @@
 ﻿
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
 
-#pragma region
+#pragma region 유클리드 호제법
+	//2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수을 얻어내는 알고리즘
 
+	int x = 24;
+	int y = 36;
+
+	int result = 0;
+	for (int i = 1; i <= x && i <= y;i++)
+	{
+		if (x % i == 0 && y % i == 0)
+		{
+			result = i;
+		}
+	}
+
+	cout << result << endl;
+
+	//vector<int> xlist;
+	//vector<int> ylist;
+
+	//for (int i = 1; i < x; i++)
+	//{
+	//	if ((x % i) == 0)
+	//	{
+	//		xlist.push_back(i);
+	//	}
+	//}
+
+	//for (int i = 1; i < y; i++)
+	//{
+	//	if ((y % i) == 0)
+	//	{
+	//		ylist.push_back(i);
+	//	}
+	//}
+
+	//int findIndex = 0;
+	//for (int i = 0; i < xlist.size(); i++)
+	//{
+	//	for (int j = 0; j < ylist.size(); j++)
+	//	{
+	//		if (xlist[i] == ylist[j])
+	//		{
+	//			findIndex = xlist[i];
+	//		}
+	//	}
+	//}
+
+
+
+	//cout << findIndex << endl;
+
+	//while (y != 0)
+	//{
+	//	int temp = x % y;
+	//	x = y;
+	//	y = temp;
+	//}
+
+	//cout << x << endl;
 
 
 
---
+++ Algorithm_5/Algorithm_5.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+
+#pragma region
+	int index[] = { 4,7,8,1,2,3,6,9,5 };
+	int size = sizeof(index) / sizeof(int);
+
+	for (int i = 1; i < size; i++)
+	{
+		int key = index[i];
+		int j = i - 1;
+
+		while (j >= 0 && index[j] > key)
+		{
+			index[j + 1] = index[j];
+			j--;
+		}
+		index[j + 1] = key;
+	}
+
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << index[i] << " ";
+	}
+
+
+#pragma endregion
+	return 0;
+}
+
+
