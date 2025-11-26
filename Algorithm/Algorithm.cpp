2348ffbb8a0c39f82133efa244886cb3ea6743b4
Karonzer <knkhsu122@naver.com>
2025-11-26 19:41:52 +0900

    작업 업로드

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,36 +1,32 @@
 ﻿#include <iostream>
-#include <vector>
 #include <unordered_map>
-using namespace std;
+#include <vector>
 
+using namespace std;
 
 template<typename T>
 class Graph
 {
 private:
-	vector<vector<T>> root;
-public:
-	Graph(int _size) : root(_size){}
-
+	unordered_map<T, vector<T>> root;
 public:
 	void insert(const T _i, const T _j)
 	{
 		root[_i].push_back(_j);
+		root[_j].push_back(_i);
 	}
 };
-
 int main()
 {
 
-#pragma region DFS
-	// 깊이 우선 탐색
-	// 그래프에서 한 방향으로 갈 수 있을 만큼 깊이 들어갔다가, 더 이상,
-	// 갈 수 없으면 다시 돌아와서 다른 경로를 탐색하는 방법입니다,
+#pragma region BFS 너비 우선 탐색
+	// 하나의 시작 정점을 방문한 후 시작 정점에 인접한
+	// 모든 정점들을 우선적으로 방문하는 탐색입니다
 
 
 #pragma endregion
-	Graph<const char*> graph(5);
-	graph.insert("A", "B");
+
+	Graph<char> graph;
 
 
 	return 0;
---
+++ Algorithm_15/Algorithm_15.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+
+template<typename T>
+class Graph
+{
+private:
+	unordered_map<T, vector<T>> root;
+	unordered_set<T> visited;
+public:
+	Graph()
+	{
+
+	}
+
+public:
+	void insert(const T _i, const T _j)
+	{
+		root[_i].push_back(_j);
+		root[_j].push_back(_i);
+	}
+
+	void search(const T _start)
+	{
+		if (auto check = visited.find(_start) == visited.end())
+		{
+			visited.insert(_start);
+			cout << _start << " ";
+			for (int i = 0; i < root[_start].size();i++)
+			{
+				search(root[_start][i]);
+			}
+		}
+	}
+};
+
+int main()
+{
+
+#pragma region DFS
+	// 깊이 우선 탐색
+	// 그래프에서 한 방향으로 갈 수 있을 만큼 깊이 들어갔다가, 더 이상,
+	// 갈 수 없으면 다시 돌아와서 다른 경로를 탐색하는 방법입니다,
+
+
+#pragma endregion
+
+	Graph<const char*> graph;
+	graph.insert("A", "B");
+	graph.insert("A", "C");
+
+	graph.insert("B", "D");
+	graph.insert("B", "E");
+
+	graph.insert("C", "F");
+	graph.insert("C", "G");
+
+	graph.search("A");
+	return 0;
+}
+
+
